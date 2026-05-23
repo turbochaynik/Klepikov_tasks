ed6728779fca781cda7c491f907d8b64937ea1a0
turbochaynik <klepikovgleb1@gmail.com>
2026-05-24 02:13:38 +0300

    Delete Swift/MovieExplorer/Swift directory

--- Swift/MovieExplorer/Swift/MineSweeper/Models/Game.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-struct Mine {
-    var isClosed: Bool = true
-    var isFlagged: Bool = false
-    var countAround: Int = 0
-    var isMine: Bool = false
-    
-}
--- Swift/MovieExplorer/Swift/WaveDefend/Models/GameConstants.swift
+++
@@ -1,9 +0,0 @@
-import CoreGraphics
-
-struct GameConstants {
-    static let playerSize = CGSize(width: 40, height: 40)
-    static let wallThickness: CGFloat = 30
-    static let joystickRadius: CGFloat = 50
-    static let joystickKnobRadius: CGFloat = 25
-    static let playerSpeed: CGFloat = 180
-}
